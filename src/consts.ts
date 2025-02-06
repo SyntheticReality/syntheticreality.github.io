46219d3f73df37a2c503dac6cb13d77cac469f6e
Syry <alexis@syry.io>
2025-02-05 20:28:20 -0500

    Updated page name and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Syry";
+export const SITE_TITLE = "Osyrys";
 export const SITE_DESCRIPTION =
-  "Experts in developing gaming, simulation, and educational VR/AR projects.";
+  "High-end VR/AR software development specialized in gaming and simulations";
